eb546f223f6c437e1dd7670e686e2585aa6912a7
calebtocco <32717912+calebtocco@users.noreply.github.com>
2025-01-10 15:59:54 -0500

    Toast Feed Notifications

--- src/toast.js
+++ src/toast.js
@@ -1,22 +1,31 @@
 export class Toast {
-  static success(title, message, time = 5000) {
-    $('#toast-nav .toast-header').removeClass('toast-error');
-    $('#toast-nav .toast-header').addClass('toast-success');
-    Toast.display(title, message, time, 'polite', 'status');
+  static success(title, message, time = 5000, url = null) {
+    document.querySelector('#toast-nav .toast-header').classList.remove('toast-error');
+    document.querySelector('#toast-nav .toast-header').classList.add('toast-success');
+    Toast.display(title, message, time, 'polite', 'status', url);
   }
 
-  static error(title, message, time = 5000) {
-    $('#toast-nav .toast-header').removeClass('toast-success');
-    $('#toast-nav .toast-header').addClass('toast-error');
-    Toast.display(title, message, time, 'assertive', 'alert');
+  static error(title, message, time = 5000, url = null) {
+    document.querySelector('#toast-nav .toast-header').classList.remove('toast-success');
+    document.querySelector('#toast-nav .toast-header').classList.add('toast-error');
+    Toast.display(title, message, time, 'assertive', 'alert', url);
   }
 
-  static display(title, message, time = 5000, politeness = 'polite', alertType = 'alert') {
-    $('#toast-nav-title').html(title);
-    $('#toast-nav-message').html(message);
-    $('#toast-nav .toast').attr('data-delay', time);
-    $('#toast-nav .toast').attr('aria-polite', politeness);
-    $('#toast-nav .toast').attr('aria-alert', alertType);
+  static display(title, message, time = 5000, politeness = 'polite', alertType = 'alert', url = null) {
+    if (url) {
+      document.querySelector('#toast-nav-link').setAttribute('href', url);
+      document.querySelector('#toast-nav-link').classList.remove('d-none');
+    } else {
+      document.querySelector('#toast-nav-link').classList.add('d-none');
+    }
+    document.querySelector('#toast-nav-title').innerHTML = title;
+    document.querySelector('#toast-nav-message').innerHTML = message;
+    
+    const toastElement = document.querySelector('#toast-nav .toast');
+    toastElement.setAttribute('data-delay', time);
+    toastElement.setAttribute('aria-polite', politeness);
+    toastElement.setAttribute('aria-alert', alertType);
+
     $('#toast-nav .toast').toast('show');
   }
 }
