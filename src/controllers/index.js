0905184ce452d10997e015eeaf9e986176d5fc3e
calebtocco <32717912+calebtocco@users.noreply.github.com>
2025-05-10 06:50:40 -0400

    Advanced Like/Text Search Filter

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,7 @@ import GlobalSearchController from './global_search_controller';
 import RemoteFormController from './remote_form_controller';
 import SearchController from './search_controller';
 import SearchDateFilterController from './search_date_filter_controller';
+import SearchLikeFilterController from './search_like_filter_controller';
 import ToastController from './toast_controller';
 
 export const radControllers = [
@@ -9,5 +10,6 @@ export const radControllers = [
   { id: 'remote-form', controller: RemoteFormController },
   { id: 'search', controller: SearchController },
   { id: 'search-date-filter', controller: SearchDateFilterController },
+  { id: 'search-like-filter', controller: SearchLikeFilterController },
   { id: 'toast', controller: ToastController }
 ];
